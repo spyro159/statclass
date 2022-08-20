d245c6c8993ad8368de3cc21f91215833d5d35c8
spyro159 <111608780+spyro159@users.noreply.github.com>
2022-08-20 12:50:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let arr = ['идет заец полесу видит медведи в пеще
 'Читаю статус: "Если у стаса болит голова, попробуйте приложить ему ко лбу новые итальянские валенки.. " Убил коммент: "А прикладывать с размаху.. или как?',
  '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
  '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
- '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+ 'когда стас захватил мир он такой говорит всем людям света - я стася справедливйы а в полте кто то крикнул стас лоох и в итоге убили диму иванова',
 ];
 
 // Получаем случайный ключ массива
